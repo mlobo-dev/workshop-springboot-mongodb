d258269f5d94d7f2be1b99afeb75f4f5470910b6
mlobo-dev <mlobo.dev@gmail.com>
2019-11-15 19:12:06 -0300

    Isert User With POST method

--- src/main/java/com/wolf/service/UserService.java
+++ src/main/java/com/wolf/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wolf.domain.User;
+import com.wolf.dto.UserDTO;
 import com.wolf.repository.UserRepository;
 import com.wolf.service.exeception.ObjectNotFoundException;
 
@@ -19,9 +20,17 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
