d7e496becb1e683a35922a7bf505261a57d88b73
mlobo-dev <mlobo.dev@gmail.com>
2019-11-15 17:57:39 -0300

    User findByID and Exception Handling

--- src/main/java/com/wolf/service/UserService.java
+++ src/main/java/com/wolf/service/UserService.java
@@ -1,12 +1,14 @@
 package com.wolf.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wolf.domain.User;
 import com.wolf.repository.UserRepository;
+import com.wolf.service.exeception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
