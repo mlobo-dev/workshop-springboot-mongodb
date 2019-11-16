ec0c2378fac5939e12c9b8d1adf53ffcc0348aa7
mlobo-dev <mlobo.dev@gmail.com>
2019-11-16 11:35:37 -0300

    Query method to find an post by title ignoring case

--- src/main/java/com/wolf/resourse/PostResource.java
+++ src/main/java/com/wolf/resourse/PostResource.java
@@ -1,73 +1,38 @@
 package com.wolf.resourse;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wolf.domain.Post;
-import com.wolf.domain.User;
-import com.wolf.dto.UserDTO;
-import com.wolf.service.UserService;
+import com.wolf.resourse.util.URL;
+import com.wolf.service.PostService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
 	@Autowired
-	private UserService service;
-
-	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll() {
-		List<User> list = service.findAll();
-		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);
-	}
+	private PostService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(new UserDTO(user));
-	}
-
-	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
-		User user = service.fromDTO(userDTO);
-		user = service.insert(user);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}
-
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> deleteById(@PathVariable String id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
-
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> insert(@PathVariable String id, @RequestBody UserDTO userDTO) {
-		User user = service.fromDTO(userDTO);
-		user.setId(id);
-		user = service.update(user);
-		return ResponseEntity.noContent().build();
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
 	}
 	
-	
-	@GetMapping(value = "/{id}/posts")
-	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(user.getPosts());
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitleIgnoreCase(@RequestParam(value="text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitleIgnoreCase(text);
+
+		return ResponseEntity.ok().body(list);
 	}
-	
+
 }
--- src/main/java/com/wolf/service/PostService.java
+++ src/main/java/com/wolf/service/PostService.java
@@ -1,5 +1,6 @@
 package com.wolf.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,13 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContaining(text);
+	}
+	
+	public List<Post> findByTitleIgnoreCase(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
