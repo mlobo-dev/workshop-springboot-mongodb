524bac271d1cac5b5f9662d5ed69a568dc78b454
mlobo-dev <mlobo.dev@gmail.com>
2019-11-16 10:58:38 -0300

    adding comments on posts

--- src/main/java/com/wolf/config/Instantiation.java
+++ src/main/java/com/wolf/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.wolf.config;
 
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.wolf.domain.Post;
 import com.wolf.domain.User;
 import com.wolf.dto.AuthorDTO;
+import com.wolf.dto.CommentDTO;
 import com.wolf.repository.PostRepository;
 import com.wolf.repository.UserRepository;
 
@@ -18,37 +20,40 @@ import com.wolf.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userReposiroty;
 
 	@Autowired
-	PostRepository postRepository;
+	private PostRepository postReposiroty;
 
 	@Override
-	public void run(String... args) throws Exception {
-
+	public void run(String... arg0) throws Exception {
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-		userRepository.deleteAll();
-		postRepository.deleteAll();
-
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				new AuthorDTO(maria));
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));
-
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		
-		userRepository.save(maria);
+		userReposiroty.save(maria);
 	}
 
 }
