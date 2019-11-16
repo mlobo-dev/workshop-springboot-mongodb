fdc14d96397692c0f7cf1397fc9b49cde78a43db
mlobo-dev <mlobo.dev@gmail.com>
2019-11-16 10:29:05 -0300

    AuthorDTO and Post refactoring

--- src/main/java/com/wolf/config/Instantiation.java
+++ src/main/java/com/wolf/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.wolf.domain.Post;
 import com.wolf.domain.User;
+import com.wolf.dto.AuthorDTO;
 import com.wolf.repository.PostRepository;
 import com.wolf.repository.UserRepository;
 
@@ -38,9 +39,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		
 	
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
