f6a83aa9313a1026a0086d9632e396916381ec3a
mlobo-dev <mlobo.dev@gmail.com>
2019-11-16 10:45:14 -0300

    End Point to return posts

--- src/main/java/com/wolf/resourse/UserResource.java
+++ src/main/java/com/wolf/resourse/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wolf.domain.Post;
 import com.wolf.domain.User;
 import com.wolf.dto.UserDTO;
 import com.wolf.service.UserService;
@@ -61,4 +62,12 @@ public class UserResource {
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
 	}
+	
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
+	
 }
